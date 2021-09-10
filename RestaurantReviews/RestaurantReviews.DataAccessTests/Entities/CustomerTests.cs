702d5b6b875c020574903a04ec0118d5451ac10f
Sean Fitzmaurice <seanfitzmaurice27@gmail.com>
2021-09-10 01:45:34 -0400

    added more tests

---
+++ RestaurantReviews/RestaurantReviews.DataAccessTests/Entities/CustomerTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using RestaurantReviews.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReviews.DataAccess.Entities.Tests
+{
+    public class CustomerTests
+    {
+        [Fact()]
+        public void CustomerTest()
+        {
+            var customer =
+                new Customer
+                {
+                    Name = "Sean",
+                    Pass = "12345",
+                    Phone = "5555555",
+                    Email = "sean@yahoo.com",
+                    IsAdmin = true
+                };
+            string actual = customer.Name;
+            string expected = "Sean";
+            Assert.Equal(actual, expected);
+            
+        }
+    }
+}
---
+++ RestaurantReviews/RestaurantReviews.DomainTests/RestaurantTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using RestaurantReviews.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReviews.Domain.Tests
+{
+    public class RestaurantTests
+    {
+        [Fact()]
+        public void RestaurantTest()
+        {
+            Restaurant restaurant = new Restaurant("Bobs", "7 arnold street", 12538);
+            restaurant.calcAvg();
+            decimal actual = restaurant.avgStars;
+            decimal expected = -1;
+            Assert.Equal(actual, expected);
+        }
+    }
+}
